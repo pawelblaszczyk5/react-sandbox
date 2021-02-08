c70571bbe410cd4d97d23bf3214727543f041897
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-02-08 21:20:44 +0100

    Header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React, {ReactNode} from 'react';
+import './Header.css';
 
 const test = <h1>test</h1>;
 
 export const Header = ({children = test, color}: { children?: ReactNode, color: string }) => {
   return (
-    <h1 style={{backgroundColor: color}}>
+    <h1 className="Header" style={{backgroundColor: color}}>
       {children}
     </h1>
   );
