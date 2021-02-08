4103b0be1a766f1dc2e1691e76472db9f65b1b21
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-02-08 21:20:54 +0100

    State button and header change

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import {Header} from './components/Header/Header';
+import {StateButton} from './components/StateButton/StateButton';
 
 const App = () => (
   <div className="App">
-    test
     <Header color="steelblue">
-      <small>test</small>
+      React-sandbox
     </Header>
+    <StateButton initialCount={5} initialIterator={3}/>
   </div>
 );
 
---
+++ src/components/StateButton/StateButton.tsx
@@ -0,0 +1,36 @@
+import React, {ChangeEvent, useState} from 'react';
+import './StateButton.css';
+
+export const StateButton = ({
+                              initialCount = 0,
+                              initialIterator = 1
+                            }: { initialCount?: number, initialIterator?: number }) => {
+  const [count, setCount] = useState(initialCount);
+  const [iterator, setIterator] = useState(initialIterator);
+
+  const increaseCount = () => {
+    setCount(count + iterator);
+  };
+
+  const handleIteratorChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setIterator(Number(e.target.value));
+  };
+
+  return (
+    <div className="StateButton">
+      <p>Current count is: {count}</p>
+      <button onClick={increaseCount}>
+        Iterate
+      </button>
+      <input type="number" value={iterator} onChange={handleIteratorChange}/>
+      <button onClick={() => {
+        setCount(initialCount);
+      }}>Reset counter
+      </button>
+      <button onClick={() => {
+        setIterator(initialIterator);
+      }}>Reset Iterator
+      </button>
+    </div>
+  );
+};
